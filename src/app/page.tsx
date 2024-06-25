28179dda1fc6b90190f8c09478aa8913a406c87b
mizphses <fuminori@mizphs.es>
2024-06-25 20:57:13 +0900

    lintfix: client dts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.scss'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h2>管理画面</h2>
+      <h2>管理画面ログイン</h2>
     </main>
   )
 }
